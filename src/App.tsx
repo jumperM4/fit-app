9eba13f7e7ace0570e0342d9c0bcbbe755f328c5
jumperM4 <pma1994ars@yandex.ru>
2023-10-18 23:34:41 +0300

    navbar menu done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
+import NavBar from "@/scenes/navbar/navbar";
+
 function App() {
-  return <div className="app">APP</div>;
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
+  return (
+    <div className="app bg-gray-20">
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
